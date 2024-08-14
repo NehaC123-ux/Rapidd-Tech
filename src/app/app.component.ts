099ea911c5a7a5d5946835a5f82d1d9413b7ee96
unknown <nehachauhan9933@gmail.com>
2024-08-14 07:58:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MasterService } from './master.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'repiddTask';
+export class AppComponent implements OnInit {
+
+  data!: any[];
+  employeeHours: { [key: string]: number } = {}; 
+
+  constructor(private readonly _master: MasterService) {}
+
+  ngOnInit(): void {
+    this.getAllDetailsRepidd();
+  }
+
+  getAllDetailsRepidd() {
+    this._master.getAllDetails().subscribe(res => {
+      this.data = res;
+      this.calculateHours(this.data);
+    });
+  }
+
+  calculateHours(data: any[]): void {
+ 
+    const hours: { [key: string]: number } = {};
+
+    data.forEach(entry => {
+      if (entry.EmployeeName) {
+        const start = new Date(entry.StarTimeUtc);
+        const end = new Date(entry.EndTimeUtc);
+        const duration = (end.getTime() - start.getTime()) / (1000 * 60 * 60);
+
+        if (!hours[entry.EmployeeName]) {
+          hours[entry.EmployeeName] = 0;
+        }
+        hours[entry.EmployeeName] += duration;
+      }
+    });
+
+    this.employeeHours = hours;
+  }
+  getEmployeeNames(): string[] {
+    return Object.keys(this.employeeHours);
+  }
+
+  isUnder100Hours(name: string): boolean {
+    return this.employeeHours[name] < 100;
+  }
 }
---
+++ src/app/master.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+  private apiUrl = 'https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code=vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==';
+
+  constructor(private http: HttpClient) { }
+
+  getAllDetails(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
